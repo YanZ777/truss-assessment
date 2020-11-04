d977f9eb79eff9ca310e353724656ce248ff5851
Yan <yanzhao777@gmail.com>
2020-11-04 00:55:10 -0800

    Switch to DataGrid
    - switch from table to datagrid since it has more of the desired functionality
    - load the data in a use effect on initial page load

--- src/PlanetsTable.js
+++ src/PlanetsTable.js
@@ -3,12 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import { DataGrid } from '@material-ui/data-grid';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -26,30 +21,46 @@ const useStyles = makeStyles((theme) => ({
    },
  }));
 
+const columns = [
+   { field: 'name', headerName: 'Name', width: 100 },
+   { field: 'climate', headerName: 'Climate', width: 100 },
+   { field: 'residents', headerName: 'Residents', width: 100 },
+   { field: 'terrain', headerName: 'Terrain', width: 150 },
+   { field: 'population', headerName: 'Population', width: 150 },
+   { field: 'surfaceArea', headerName: 'Surface Area', width: 50 },
+];
+
 function PlanetsTable() {
    const classes = useStyles();
-   const [allPlanetData, setAllPlanetData] = React.useState({});
-
-   const createPlanetRow  = (planetData) => {
-
-   } 
-
-   const handleClick = (event) => {
-      Axios.get("https://swapi.dev/api/planets/")
+   const [allPlanetDataRows, setAllPlanetDataRows] = React.useState([]);
+   
+   React.useEffect(
+      () => {
+         Axios.get("https://swapi.dev/api/planets/")
          .then((response) => {
-            console.log(response);
+            const results = response.data.results;
+            console.log(results);
+            const rows = results.map((planet, index) => {
+               return {
+                  id: index,
+                  name: planet.name,
+                  climate: planet.climate !== 'unknown' ? planet.climate : '?',
+                  residents: 0,
+                  terrain: planet.terrain !== 'unknown' ? planet.terrain : '?',
+                  population: planet.population !== 'unknown' ? planet.population : '?',
+                  surfaceArea: 0,
+               };
+            });
+            setAllPlanetDataRows(rows);
          });
-   }
+      },
+      []
+   );
 
     return (
-      <IconButton 
-         edge="start"
-         className={classes.menuButton}
-         color="inherit"
-         aria-label="menu"
-         onClick={handleClick}>
-         <MenuIcon />
-      </IconButton>
+      <div style={{ height: 400, width: '100%' }}>
+         <DataGrid rows={allPlanetDataRows} columns={columns} pageSize={10} />
+      </div>
     );
 };
 
